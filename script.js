afa23c87fd302539ee14578d4f9ced895a6519a0
Nguyễn Thanh Hải <85782033+NguyenThanhHai-palat@users.noreply.github.com>
2021-07-05 14:23:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-function saveAs(dataToDownload,filename) {
- var element = document.createElement('a');
- element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(dataToDownload));
- element.setAttribute('download', filename);
- element.style.display = 'none';
- document.body.appendChild(element);
- element.click();
- document.body.removeChild(element);
-}
-function download() {
- var text = document.getElementById('downloadMe').innerText;
- var file = '/The Cup Of Life.mp3';
- saveAs(mp3,file,var)
-}
+  var d = new Date();
+     var thu = new Array(7);
+    thu[0] = "Chủ Nhật,";
+    thu[1] = "Thứ Hai,";
+    thu[2] = "Thứ Ba,";
+    thu[3] = "Thứ Tư,";
+    thu[4] = "Thứ Năm,";
+    thu[5] = "Thứ Sáu,";
+    thu[6] = "Thứ Bảy,";
+
+  var n = thu[d.getDay()];
+  document.getElementById("thu").innerHTML = n;
+  
+ var ngay = new Date().getDate();
+      document.getElementById("ngay").innerHTML = ngay;
+
+     var gio = new Date().getHours();
+     document.getElementById("gio").innerHTML = gio;
+
+    var phut = new Date().getMinutes()
+    document.getElementById("phut").innerHTML = phut;
+
+var thang = new Array();
+  thang[0] = "Một";
+  thang[1] = "Hai";
+  thang[2] = "Ba";
+  thang[3] = "Tư";
+  thang[4] = "Năm";
+  thang[5] = "Sáu";
+  thang[6] = "Bảy";
+  thang[7] = "Tám";
+  thang[8] = "Chín";
+  thang[9] = "Mười";
+  thang[10] = "Mười Một";
+  thang[11] = "Mười Hai";
+  var d = new Date();
+  var n = thang[d.getMonth()];
+  document.getElementById("thang").innerHTML = n;
